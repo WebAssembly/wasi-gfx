b36ffbd0ab9fd46773c30b891539f2c631e90c8e
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2025-04-02 14:48:39 -0400

    Update internal imports

--- install-wit-deps.sh
+++ install-wit-deps.sh
@@ -1,4 +1,4 @@
-wit-deps -m webgpu/deps.toml -l webgpu/deps.lock -d webgpu/deps
-wit-deps -m surface/deps.toml -l surface/deps.lock -d surface/deps
-wit-deps -m graphics-context/deps.toml -l graphics-context/deps.lock -d graphics-context/deps
-wit-deps -m frame-buffer/deps.toml -l frame-buffer/deps.lock -d frame-buffer/deps
+wit-deps -m webgpu/deps.toml -l webgpu/deps.lock -d webgpu/deps update
+wit-deps -m surface/deps.toml -l surface/deps.lock -d surface/deps update
+wit-deps -m graphics-context/deps.toml -l graphics-context/deps.lock -d graphics-context/deps update
+wit-deps -m frame-buffer/deps.toml -l frame-buffer/deps.lock -d frame-buffer/deps update

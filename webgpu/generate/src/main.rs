6c59a46f3360dd812f751cf2a40525f2579d5dbf
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2025-04-02 13:54:05 -0400

    Version 0.0.1 🎉

--- webgpu/generate/src/main.rs
+++ webgpu/generate/src/main.rs
@@ -4,39 +4,51 @@ use webidl2wit::{ConversionOptions, ResourceInheritance};
 fn main() {
     let webidl = fs::read_to_string(Path::new("./webgpu-spec/spec/webgpu.idl")).unwrap();
     let webidl_ast = weedle::parse(&webidl).unwrap();
-    let wit_ast = webidl2wit::webidl_to_wit(webidl_ast, ConversionOptions {
-        phantom_interface: vec![
-            "Navigator".into(),
-            "WorkerNavigator".into(),
-            "HTMLVideoElement".into(),
-            "HTMLImageElement".into(),
-            "VideoFrame".into(),
-            "ImageBitmap".into(),
-            "ImageData".into(),
-            "EventTarget".into(),
-            "DOMException".into(),
-            "Event".into(),
-        ],
-        phantom_dictionaries: vec![
-            "EventInit".into(),
-            "PredefinedColorSpace".into(),
-            "HTMLCanvasElement".into(),
-            "OffscreenCanvas".into(),
-        ],
-        resource_inheritance: ResourceInheritance::DuplicateMethods,
-        package_name: webidl2wit::PackageName::new("wasi", "webgpu", None),
-        interface_name: "webgpu".into(),
-        ..Default::default()
-    },).unwrap();
+    let wit_ast = webidl2wit::webidl_to_wit(
+        webidl_ast,
+        ConversionOptions {
+            phantom_interface: vec![
+                "Navigator".into(),
+                "WorkerNavigator".into(),
+                "HTMLVideoElement".into(),
+                "HTMLImageElement".into(),
+                "VideoFrame".into(),
+                "ImageBitmap".into(),
+                "ImageData".into(),
+                "EventTarget".into(),
+                "DOMException".into(),
+                "Event".into(),
+            ],
+            phantom_dictionaries: vec![
+                "EventInit".into(),
+                "PredefinedColorSpace".into(),
+                "HTMLCanvasElement".into(),
+                "OffscreenCanvas".into(),
+            ],
+            resource_inheritance: ResourceInheritance::DuplicateMethods,
+            package_name: webidl2wit::PackageName::new(
+                "wasi",
+                "webgpu",
+                Some(semver::Version {
+                    major: 0,
+                    minor: 0,
+                    patch: 1,
+                    pre: Default::default(),
+                    build: Default::default(),
+                }),
+            ),
+            interface_name: "webgpu".into(),
+            ..Default::default()
+        },
+    )
+    .unwrap();
     let wit = wit_ast.to_string();
-    
+
     let mut resolve = wit_parser::Resolve::new();
     // TODO: remove once we have streams
     resolve.push_file(format!("../deps/io/poll.wit")).unwrap();
 
-    resolve
-        .push_str("", &wit)
-        .unwrap();
+    resolve.push_str("", &wit).unwrap();
 
     let mut packages = wit_encoder::packages_from_parsed(&resolve);
 
@@ -53,7 +65,9 @@ fn main() {
     let item = package.items_mut().remove(0);
     let interface = match item {
         wit_encoder::PackageItem::Interface(interface) => interface,
-        wit_encoder::PackageItem::World(_) => panic!("Package should contain exactly one interface"),
+        wit_encoder::PackageItem::World(_) => {
+            panic!("Package should contain exactly one interface")
+        }
     };
     let transforms = serde_json::from_str(include_str!("../webgpu-transforms.json")).unwrap();
     let interface = wit_transforms::transform(interface, transforms);
